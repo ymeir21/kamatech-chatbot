2cb4bffb5b5bb0332d2c44bdb71a5fa6264c029f
Yacov <yacov@yacov.yacov>
2021-11-04 06:09:57 +0200

    Waiting for Scheduler implementation

--- Runners/ConsoleApp/Program.cs
+++ Runners/ConsoleApp/Program.cs
@@ -1,15 +1,30 @@
 ﻿using BasePlugin;
 using BasePlugin.Interfaces;
 using BasePlugin.Records;
+using CountDown;
 using DiceRoller;
 using System;
 using System.Collections.Generic;
 
 namespace ConsoleApp
 {
+    class Scheduler : IScheduler
+    {
+        public void Schedule(TimeSpan ts, string data)
+        {
+            throw new NotImplementedException("You guys should implement this");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
+        {
+            var game1 = new CountDownPlugin(new Scheduler()) as IPlugin;
+            game1.Execute("", null, null);
+        }
+
+        static void RunDiceRoller()
         {
             var game1 = new DiceRollerPlugin() as IPlugin;
             var history = new List<PluginOutput>();
--- Shared/BasePlugin/Interfaces/IScheduler.cs
+++ Shared/BasePlugin/Interfaces/IScheduler.cs
@@ -5,6 +5,5 @@ namespace BasePlugin.Interfaces
     public interface IScheduler
     {
         void Schedule(TimeSpan ts, string data);
-        void Schedule(DateTime date, string data);
     }
 }
