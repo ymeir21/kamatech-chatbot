21ef0099c5392fb95cb951d3097998a735f659e9
Arye Holtzberg <AryeHoltzberg@gmail.com>
2021-11-04 12:01:27 +0200

    Add an option to the countDown plugin to get a number of seconds as a string and count down accordingly

--- Plugins/CountDown/CountDownPlugin.cs
+++ Plugins/CountDown/CountDownPlugin.cs
@@ -17,8 +17,22 @@ namespace CountDown
 
         public PluginOutput Execute(string str, IList<PluginOutput> pluginHistory, ICallbacks callbacks)
         {
-            _scheduler.Schedule(TimeSpan.FromSeconds(1), "");
-            return new PluginOutput("Countdown started.", null);
+            if str=="" // i.e start 1 second default CountDown 
+            { 
+                _scheduler.Schedule(TimeSpan.FromSeconds(1), "");
+                return new PluginOutput("Countdown started.", null);
+             }else { 
+             try
+            {
+                    var interval = Int32.Parse(input);
+                    _scheduler.Schedule(TimeSpan.FromSeconds(interval), "");
+                    return new PluginOutput("Countdown started.", null);
+             }
+        catch (FormatException)
+        {
+            return new PluginOutput("Countdown failed, string input nust represent vaild seconds.", null);
+        }
+            }
         }
     }
 }
