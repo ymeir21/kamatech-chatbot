915051a9bfb0110a70c327e4d255111d7817ddf1
Arye Holtzberg <AryeHoltzberg@gmail.com>
2021-11-07 19:35:27 +0200

    fix the idention

--- Plugins/CountDown/CountDownPlugin.cs
+++ Plugins/CountDown/CountDownPlugin.cs
@@ -17,12 +17,13 @@ namespace CountDown
 
         public PluginOutput Execute(string str, IList<PluginOutput> pluginHistory, ICallbacks callbacks)
         {
-            if (str == "") // i.e start 1 second default CountDown  
+            if(str == "") // i.e start 1 second default CountDown  
             { 
                 _scheduler.Schedule(TimeSpan.FromSeconds(1), "");
                 return new PluginOutput("Countdown started.", null);
              }
-            else { 
+            else 
+            { 
                  try
                 {
                   var interval = Int32.Parse(str);
