7c89815c79c9373e1e942bf53061f312af3651b3
YacovGold <8984042+YacovGold@users.noreply.github.com>
2021-11-25 13:20:12 +0200

    Change name

--- Shared/Dal/Dal/IDal.cs
+++ Shared/Dal/Dal/IDal.cs
@@ -9,9 +9,9 @@ namespace Dal
 {
     public interface IDal
     {
-        
+
         /// <param name="userData">Phone number etc.</param>
-        void SaveData(string userData, string pluginId, string data);
+        void SaveData2(string userData, string pluginId, string data);
         /// <param name="userData">Phone number etc.</param>
         string LoadData(string userData, string pluginId);
     }
