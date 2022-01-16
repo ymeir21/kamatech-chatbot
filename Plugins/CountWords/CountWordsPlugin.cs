2527cb1b13ff13007091cb3b60aaa8d0db7d47a4
meir@ccc.co.il <meir@ccc.co.il>
2022-01-16 11:55:37 +0200

    Add count words plugin

---
+++ Plugins/CountWords/CountWordsPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using BasePlugin;
+using BasePlugin.Interfaces;
+using BasePlugin.Records;
+
+
+namespace CountWords
+{
+    record Session(int NumberOfWords);
+    public class CountWordsPlugin : IPlugin
+    {
+        public string Id => throw new NotImplementedException();
+
+
+        public bool CanExecute(string input, string session)
+        {
+            return true;
+        }
+
+        public PluginOutput Execute(string input, string session, ICallbacks callbacks)
+        {
+            int numberOfWords = input.Split(' ').ToList().Where(s => !string.IsNullOrEmpty(s)).Count();
+            var ses = new Session(numberOfWords);
+            return new PluginOutput("The number of words is " + numberOfWords, JsonSerializer.Serialize(ses)); 
+        }
+    }
+}
